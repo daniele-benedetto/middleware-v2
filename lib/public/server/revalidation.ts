6eecbb3052bf228ef2cb82900b7b4252882f4ed8
Daniele <daniele.benedetto@outlook.it>
2026-06-24 10:41:37 +0200

    feat: include PUBLIC_ISSUES_ARCHIVE_CACHE_TAG in revalidation functions

--- lib/public/server/revalidation.ts
+++ lib/public/server/revalidation.ts
@@ -5,6 +5,7 @@ import { revalidateTag } from "next/cache";
 import { PUBLIC_ARTICLE_PAGE_CACHE_TAG } from "@/lib/public/server/article-page";
 import { PUBLIC_HOME_CACHE_TAG } from "@/lib/public/server/home";
 import { PUBLIC_ISSUE_PAGE_CACHE_TAG } from "@/lib/public/server/issue-page";
+import { PUBLIC_ISSUES_ARCHIVE_CACHE_TAG } from "@/lib/public/server/issues-archive";
 import { PUBLIC_PAGE_CACHE_TAG } from "@/lib/public/server/page";
 
 function revalidatePublicTag(tag: string) {
@@ -15,12 +16,14 @@ export function revalidatePublicArticleContent() {
   revalidatePublicTag(PUBLIC_ARTICLE_PAGE_CACHE_TAG);
   revalidatePublicTag(PUBLIC_HOME_CACHE_TAG);
   revalidatePublicTag(PUBLIC_ISSUE_PAGE_CACHE_TAG);
+  revalidatePublicTag(PUBLIC_ISSUES_ARCHIVE_CACHE_TAG);
 }
 
 export function revalidatePublicIssueContent() {
   revalidatePublicTag(PUBLIC_HOME_CACHE_TAG);
   revalidatePublicTag(PUBLIC_ISSUE_PAGE_CACHE_TAG);
   revalidatePublicTag(PUBLIC_ARTICLE_PAGE_CACHE_TAG);
+  revalidatePublicTag(PUBLIC_ISSUES_ARCHIVE_CACHE_TAG);
 }
 
 export function revalidatePublicPageContent() {
